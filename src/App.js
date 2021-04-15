e1ae895e8a990034aecaa159daa56a3d41911c06
cafrmen <cafrmen@users.noreply.github.com>
2021-04-15 18:37:48 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./book-solid.svg";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="title">
+        <img src={ logo } alt="open book" className="logo" />
+        <h1>DICTIONARY</h1> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
